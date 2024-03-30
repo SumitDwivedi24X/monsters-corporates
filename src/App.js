6426dfbb440c0eff3d79c61e5b458feac6af34e7
SumitDwivedi24X <sumitdwivedi1100@gmail.com>
2024-03-30 16:46:00 +0530

    Switched from class to functional component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,47 @@
-import { Component } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component.jsx";
 import SearchBox from "./components/search-box/search-box.component.jsx";
+import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
-
-  componentDidMount() {
+const App = () => {
+  const [searchField, setSearchField] = useState(""); //[value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []); // to save it from infinite re-rendering of fetch request,
+  //and we don't have to pass anything to re-render it again.
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField); //'includes' is case sensitive(l!=L), using toLocaleLowercase
     });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [
+    monsters,// whenever monsters list changes then only it will re-render
+    searchField,// and also whenever searchField changes
+  ]); /* to save it from re-rendering whenever other function occurs, beacuse whole app 
+  re-renders which also renders this function results in extra consumption of cpu */
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField); //'includes' is case sensitive(l!=L)
-    }); // so that's why we are using toLocaleLowercase
-    return (
-      <div className="App">
-        <h1 className="app-title">MONSTERS CORPORATES</h1>
-        <SearchBox
-          className="monsters-search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters..."
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-      // In components, we can name props to any name, eg. monsters
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title">MONSTERS CORPORATES</h1>
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters..."
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+    // In components, we can name props to any name, eg. monsters
+  );
+};
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,20 +1,18 @@
-import { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const {name, email, id} = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=160x160`}
-          loading="lazy"
-        />
-        <h3>{name}</h3>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ monster }) => {
+  const { name, email, id } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=160x160`}
+        loading="lazy"
+      />
+      <h3>{name}</h3>
+      <p>{email}</p>
+    </div>
+  );
+};
+
 export default Card;
